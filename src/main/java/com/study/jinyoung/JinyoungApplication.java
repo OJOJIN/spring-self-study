9e909a9e59d5d48b3a23af7f61b7283d558ad2e9
OJOJIN <ojy101039@gmail.com>
2023-10-01 00:26:44 +0900

    init

---
+++ src/main/java/com/study/jinyoung/JinyoungApplication.java
@@ -0,0 +1,13 @@
+package com.study.jinyoung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinyoungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinyoungApplication.class, args);
+	}
+
+}
