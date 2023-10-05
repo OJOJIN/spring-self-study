4db25afb69414cdfbb6d9d133414c8f9e1886e5b
OJOJIN <ojy101039@gmail.com>
2023-10-05 15:14:46 +0900

    [feat] token constants

---
+++ src/main/java/com/study/jinyoung/common/auth/AuthConstants.java
@@ -0,0 +1,6 @@
+package com.study.jinyoung.common.auth;
+
+public final class AuthConstants {
+    public static final String AUTH_HEADER = "Authorization";
+    public static final String TOKEN_TYPE = "Bearer ";
+}
