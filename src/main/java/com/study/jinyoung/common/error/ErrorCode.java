748bc17fe2bd196d7ef8edd3eb3a960cfbbcc476
OJOJIN <ojy101039@gmail.com>
2023-12-05 16:50:21 +0900

    [feat] Exception handler 추가

--- src/main/java/com/study/jinyoung/common/error/ErrorCode.java
+++ src/main/java/com/study/jinyoung/common/error/ErrorCode.java
@@ -11,11 +11,13 @@ public enum ErrorCode {
     /**
      *  400 Bad Request
      */
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
 
 
     /**
      *  401 Unauthorized
      */
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "리소스 접근 권한이 없습니다."),
 
     /**
      *  403 Forbidden
@@ -25,7 +27,12 @@ public enum ErrorCode {
     /**
      *  404 Not Found
      */
+    ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "엔티티를 찾을 수 없습니다."),
 
+    /**
+     * 405 Method Not Allowed
+     */
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "잘못된 HTTP method 요청입니다."),
 
     /**
      * 409 Conflict
