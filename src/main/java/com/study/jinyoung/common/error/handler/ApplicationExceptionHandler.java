56036a495b93d9187e02ff0dff520e91ba6b7daa
OJOJIN <ojy101039@gmail.com>
2023-12-05 16:48:28 +0900

    [feat] Exception handler 추가

--- src/main/java/com/study/jinyoung/common/error/handler/ApplicationExceptionHandler.java
+++ src/main/java/com/study/jinyoung/common/error/handler/ApplicationExceptionHandler.java
@@ -1,20 +1,66 @@
 package com.study.jinyoung.common.error.handler;
 
 import com.study.jinyoung.common.error.ApplicationException;
+import com.study.jinyoung.common.error.ErrorCode;
 import com.study.jinyoung.common.error.dto.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Slf4j
 @RestControllerAdvice
 public class ApplicationExceptionHandler {
 
+    /**
+     * Valid & Validated annotation의 binding error를 handling합니다.
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error(">>> handle: MethodArgumentNotValidException ", e);
+        final ErrorResponse errorBaseResponse = ErrorResponse.of(ErrorCode.BAD_REQUEST);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorBaseResponse);
+    }
+
+    /**
+     * ModelAttribute annotation의 binding error를 handling합니다.
+     */
+    @ExceptionHandler(BindException.class)
+    protected ResponseEntity<ErrorResponse> handleBindException(BindException e) {
+        log.error(">>> handle: BindException ", e);
+        final ErrorResponse errorBaseResponse = ErrorResponse.of(ErrorCode.BAD_REQUEST);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorBaseResponse);
+    }
+
+    /**
+     * RequestParam annotation의 binding error를 handling합니다.
+     */
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+        log.error(">>> handle: MethodArgumentTypeMismatchException ", e);
+        final ErrorResponse errorBaseResponse = ErrorResponse.of(ErrorCode.BAD_REQUEST);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorBaseResponse);
+    }
+
+    /**
+     * 지원하지 않는 HTTP method로 요청 시 발생하는 error를 handling합니다.
+     */
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        log.error(">>> handle: HttpRequestMethodNotSupportedException ", e);
+        final ErrorResponse errorBaseResponse = ErrorResponse.of(ErrorCode.METHOD_NOT_ALLOWED);
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(errorBaseResponse);
+    }
+
     @ExceptionHandler(ApplicationException.class)
     protected ResponseEntity<ErrorResponse> handleApplicationException(ApplicationException exception) {
         log.info("{}: {}", exception.getClass().getSimpleName(), exception.getMessage(), exception);
-        return ResponseEntity.status(exception.getStatus()).body(ErrorResponse.of(exception));
+        return ResponseEntity.status(exception.getError().getStatus()).body(ErrorResponse.of(exception.getError()));
     }
 
     @ExceptionHandler(Exception.class)
